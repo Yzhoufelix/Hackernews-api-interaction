d9d9f0f3b8e1ba11c139d0824916b272b535bb99
Yzhoufelix <felix.zhouyf.ca@gmail.com>
2019-07-07 10:46:39 -0400

    Paginated Fetch

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import "./App.css";
 
 const DEFAULT_QUERY = "redux";
+const DEFAULT_HPP = "50";
 const PATH_BASE = "https://hn.algolia.com/api/v1";
 const PATH_SEARCH = "/search";
 const PARAM_SEARCH = "query=";
+const PARAM_PAGE = "page=";
+const PARAM_HPP = "hitsPerPage=";
 
-const isSearched = searchTerm => item =>
-  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+// const isSearched = searchTerm => item =>
+//   item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
   constructor(props) {
@@ -19,25 +22,37 @@ class App extends Component {
     this.handleDismiss = this.handleDismiss.bind(this);
     this.handleSearchChange = this.handleSearchChange.bind(this);
     this.setSearchStories = this.setSearchStories.bind(this);
+    this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
   }
 
   componentDidMount() {
     const { searchTerm } = this.state;
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+    this.fetchSearchStories(searchTerm);
+  }
+
+  fetchSearchStories(searchTerm, page = 0) {
+    fetch(
+      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
+    )
       .then(response => response.json())
       .then(result => this.setSearchStories(result))
       .catch(error => error);
   }
 
   setSearchStories(result) {
+    const { hits, page } = result;
+    const oldHits = page === 0 ? [] : this.state.result.hits; // paginated hits
+    const updatedHits = [...oldHits, ...hits];
     this.setState({
-      result
+      result: { ...this.state.result, hits: updatedHits, page }
     });
   }
 
   handleDismiss(id) {
+    const { result } = this.state;
+    const updatedHits = result.hits.filter(item => item.objectID !== id);
     this.setState({
-      result: [...this.state.result.filter(item => item.objectID !== id)]
+      result: Object.assign({}, result, { hits: updatedHits })
     });
   }
 
@@ -48,8 +63,15 @@ class App extends Component {
     });
   }
 
+  handleSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.fetchSearchStories(searchTerm);
+    event.preventDefault();
+  }
+
   render() {
     const { searchTerm, result } = this.state;
+    const page = result ? result.page : 0;
 
     return (
       <div className="page">
@@ -57,17 +79,20 @@ class App extends Component {
           className="interactions"
           value={searchTerm}
           onChange={this.handleSearchChange}
+          onSubmit={this.handleSearchSubmit}
         >
           Search
         </Search>
 
         {result ? (
-          <Table
-            pattern={searchTerm}
-            result={result.hits}
-            onDismiss={this.handleDismiss}
-          />
+          <Table result={result.hits} onDismiss={this.handleDismiss} />
         ) : null}
+
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchStories(searchTerm, page + 1)}>
+            More
+          </Button>
+        </div>
       </div>
     );
   }
@@ -76,23 +101,23 @@ class App extends Component {
 export default App;
 
 const Search = props => {
-  const { value, onChange, children, className } = props;
+  const { value, onChange, children, className, onSubmit } = props;
   return (
-    <form className={className}>
-      {children}
+    <form className={className} onSubmit={onSubmit}>
       <input type="text" value={value} onChange={onChange} />
+      <button type="submit">{children}</button>
     </form>
   );
 };
 
 const Table = props => {
-  const { pattern, result, onDismiss } = props;
+  const { result, onDismiss } = props;
   const largeColumn = { width: "40%" };
   const midColumn = { width: "30%" };
   const smallColumn = { width: "10%" };
   return (
     <div className="table">
-      {result.filter(isSearched(pattern)).map(item => {
+      {result.map(item => {
         return (
           <div key={item.objectID} className="table-row">
             <span style={largeColumn}>
